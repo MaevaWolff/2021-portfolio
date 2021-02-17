095b397e7b3853867e454b6abe36c9904bd8d058
MaevaWolff <maeva.wolff@hetic.net>
2021-02-17 01:14:45 +0100

    add theme on app

---
+++ src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#F8572E",
+    black: "#171A21",
+    white: "#f1f1f1",
+  },
+};
+
+export default theme;
