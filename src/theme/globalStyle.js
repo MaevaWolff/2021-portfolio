9a6336f82957aba9f906d07a518a2a75ee1de14d
MaevaWolff <maeva.wolff@hetic.net>
2021-02-18 18:16:01 +0100

    import fonts

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import device from "./device";
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Red+Rose:wght@300;400;700&display=swap');
 
 html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
   margin: 0;
@@ -43,7 +42,7 @@ a{
 body{
   width: 100%;
   min-height: 100vh;
-  font-family: "Red Rose";
+  font-family: 'RedRose';
   background-color: ${(props) => props.theme.colors.black};
   color: ${(props) => props.theme.colors.white};
   font-weight: lighter;
