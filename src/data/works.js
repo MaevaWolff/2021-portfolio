b92fbcdff463b88207c217723c541f54a326677a
MaevaWolff <maeva.wolff@hetic.net>
2021-02-23 01:36:06 +0100

    add all projects

--- src/data/works.js
+++ src/data/works.js
@@ -1,20 +1,39 @@
-import KatanaImg from "../assets/katana-searching.png";
+import KatanaSearchImg from "../assets/katana-searching.png";
+import KatanaImg from "../assets/katana.png";
+import PizzaImg from "../assets/pizza.png";
+import LinaImg from "../assets/lina.png";
 
 export const works = [
   {
-    id: "0",
-    subtitle: "REACT-MOVIE-SEARCHING",
+    id: 0,
+    subtitle: "REACT-MOVIE-SEARCH",
     name: "KATANA-SEARCHING",
     stack: "ReactJS",
     link: "/",
-    image: KatanaImg,
+    image: KatanaSearchImg,
   },
   {
-    id: "1",
+    id: 1,
     subtitle: "NETFLIX LIKE",
     name: "KATANA",
     stack: "PHP",
     link: "/",
     image: KatanaImg,
+  },
+  {
+    id: 2,
+    subtitle: "FOOD",
+    name: "PIZZA",
+    stack: "DESIGN",
+    link: "/",
+    image: PizzaImg,
+  },
+  {
+    id: 3,
+    subtitle: "PORTFOLIO",
+    name: "LINA",
+    stack: "ReactJS",
+    link: "/",
+    image: LinaImg,
   },
 ];
